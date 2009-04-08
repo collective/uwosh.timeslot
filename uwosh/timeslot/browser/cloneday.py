d252f64ee923c0656445d34482f634afa6188c06
Marshall Scorcio <marshall.scorcio@gmail.com>
2009-04-08 21:23:58 +0000

    Everything works!

--- uwosh/timeslot/browser/cloneday.py
+++ uwosh/timeslot/browser/cloneday.py
@@ -4,6 +4,7 @@ from Products.CMFCore import utils as cmfutils
 from Products.Five.browser import pagetemplatefile
 from Products.Five.formlib import formbase
 
+from zExceptions import BadRequest
 
 class ICloneDay(interface.Interface):
     numToCreate = schema.Int(title=u'Number to Create', description=u'The number of clones to create', required=True)
@@ -11,18 +12,37 @@ class ICloneDay(interface.Interface):
 class CloneDayForm(formbase.PageForm):
     form_fields = form.FormFields(ICloneDay)
     result_template = pagetemplatefile.ZopeTwoPageTemplateFile('cloneday-results.pt')
+    success = True
+    errors = []
 
     @form.action('Clone')
     def action_migrate(self, action, data):
+        self.success = True
+        self.errors = []
         numToCreate = data['numToCreate']
         day = self.context
-        signupSheet = day.aq_parent
+        origId = day.id
+        origTitle = day.Title()
+        signupSheet = day.aq_inner.aq_parent   
         
-        dayCopy = signupSheet.manage_copyObjects([day])
+        dayContentsCopy = day.manage_copyObjects(day.objectIds())
+        
         for i in range(0, numToCreate):
-            signupSheet.manage_pasteObjects(dayCopy)
+            newId = '%s-%d' % (origId, i)
+            newTitle = '%s-%d' % (origTitle, i)
+            
+            try:
+                signupSheet.invokeFactory(id=newId, type_name='Day')
+            except BadRequest:
+                self.success = False
+                self.errors.append("Operation failed because there is already an object named: %s" % newId)
+                break
+                
+            newDay = signupSheet[newId]
+            newDay.setTitle(newTitle)
+            newDay.manage_pasteObjects(dayContentsCopy)
+            newDay.removeAllPeople()
+            newDay.reindexObject()
             
         return self.result_template()
         
-        
-  
