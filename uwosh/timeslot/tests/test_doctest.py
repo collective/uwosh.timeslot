0b1c954b23e41a8e40694743857319ac5ad5da5e
Marshall Scorcio <marshall.scorcio@gmail.com>
2009-04-13 22:13:07 +0000

    Finally got tests to work. Now to go about writing some.

---
+++ uwosh/timeslot/tests/test_doctest.py
@@ -0,0 +1,11 @@
+import unittest
+from Testing import ZopeTestCase as ztc
+
+from uwosh.timeslot.tests.base import UWOshTimeslotFunctionalTestCase
+        
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(ztc.FunctionalDocFileSuite(
+                      'browser.txt', package='uwosh.timeslot.tests',
+                      test_class=UWOshTimeslotFunctionalTestCase))
+    return suite
