8db781695f2d361ee2e6de33ab1b5869cef3ab71
Marshall Scorcio <marshall.scorcio@gmail.com>
2009-04-13 22:25:03 +0000

    Fixed version number. Really added the tests.

---
+++ uwosh/timeslot/tests/base.py
@@ -0,0 +1,27 @@
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+
+from Testing import ZopeTestCase as ztc
+
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import onsetup
+
+@onsetup
+def setup_uwosh_timeslot():
+    """Set up the additional products required for uwosh.timeslot"""
+    
+    fiveconfigure.debug_mode = True
+    import uwosh.timeslot
+    zcml.load_config('configure.zcml', uwosh.timeslot)
+    fiveconfigure.debug_mode = False
+    
+    ztc.installPackage('uwosh.timeslot')
+    
+setup_uwosh_timeslot()
+ptc.setupPloneSite(products=['uwosh.timeslot'])
+
+class UWOshTimeslotTestCase(ptc.PloneTestCase):
+    """A base calss for all the tests in this packages."""
+    
+class UWOshTimeslotFunctionalTestCase(UWOshTimeslotTestCase, ptc.FunctionalTestCase):
+    pass
