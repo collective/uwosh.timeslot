38da98458c1f86d90fa8a46a3938be9d4512c616
Marshall Scorcio <marshall.scorcio@gmail.com>
2009-06-04 21:07:18 +0000

    Major refactoring of choosetimeslot. Amazingly all the tests still pass.

--- uwosh/timeslot/browser/choosetimeslot.py
+++ uwosh/timeslot/browser/choosetimeslot.py
@@ -26,102 +26,95 @@ class ChooseTimeSlot(BrowserView):
         return getToolByName(self.context, 'portal_url').getPortalObject()
     
     def submitUserSelection(self):
-        success = False
-        waiting = True
-        validEmail = False
-        errorMsg = ''
+        self.success = False
+        self.waiting = True
+        self.errorMsg = ''
     
-    	userInfo = self.getUserInput()
-    	userInfo.update(self.getMemberInfo())
-    
-        (anyMissing, missingFields) = self.areAnyRequiredFieldsMissing(userInfo)   
-        if anyMissing:
-            success = False
-            errorMsg = 'You did not complete the following fields: '
-            errorMsg += ', '.join(missingFields)
+    	self.getUserInput()
+    	self.getMemberInfo()
+               
+        if self.areAnyRequiredFieldsEmpty():
+            self.success = False
+            self.errorMsg = 'You did not complete the following fields: '
+            self.errorMsg += ', '.join(self.getListOfEmptyRequiredFields())
         
         else:
-            for slot in userInfo['selectedSlots']:
-                (signupSheet, date, time) = slot.split(' @ ')
-                signupSheet = self.context.getSignupSheet(signupSheet)
-                day = signupSheet.getDay(date)
-                timeSlot = day.getTimeSlot(time)
-                allowWaitingList = timeSlot.getAllowWaitingList()
-                numberOfAvailableSpots = timeSlot.getNumberOfAvailableSpots()
-                
-                isEmail = validation.validatorFor('isEmail')
-                validEmail = isEmail(userInfo['email']) == 1
-                
-                if allowWaitingList or numberOfAvailableSpots > 0:
-                    person = self.createPerson(timeSlot, userInfo)
-                
-                    if numberOfAvailableSpots > 0:
-                        self.signupPerson(person)
-                        waiting = False
-                    elif validEmail:
-                        self.sendWaitingConfirmationEmail(userInfo, signupSheet, slot)
-                    
-                    success = True
-            
-                else:
-                    errorMsg = 'The slot you selected is already full. Please select a different one'
-                    success = False
+            for slotLabel in self.selectedSlots:
+                self.getSlotAndSignUserUpForIt(slotLabel)
         
         self.request.response.redirect(self.context.absolute_url() + 
-                                       '/signup-results?success=%d&waiting=%d&emailSent=%d&errorMsg=%s' % (success,waiting,validEmail,errorMsg))
+                                       '/signup-results?success=%d&waiting=%d&emailSent=%d&errorMsg=%s' % (self.success, self.waiting, self.isEmailValid(), self.errorMsg))
 
 
     def getUserInput(self):
-        userInput = dict()
-        userInput['phone'] = self.request.get('phone', '')
-        userInput['classification'] = self.request.get('classification', '')
-        userInput['dept'] = self.request.get('dept', '')
-        userInput['selectedSlots'] = self.request.get('slotSelection', None)
+        for (field, _) in self.context.schema['extraFields'].vocabulary:
+            setattr(self, field, self.request.get(field, ''))
 
-        if type(userInput['selectedSlots']) != list:
-            userInput['selectedSlots'] = [userInput['selectedSlots']]
+        self.selectedSlots = self.request.get('slotSelection', None)
 
-        return userInput
+        if type(self.selectedSlots) != list:
+            self.selectedSlots = [self.selectedSlots]
 		
     def getMemberInfo(self):
-    	memberInfo = dict()
         portal_membership = getToolByName(self, 'portal_membership')
         member = portal_membership.getAuthenticatedMember()
-        memberInfo['username'] = member.getUserName()
-        memberInfo['fullname'] = member.getProperty('fullname')
-        if memberInfo['fullname'] == '':
-            memberInfo['fullname'] = memberInfo['username'] 
-        memberInfo['email'] = member.getProperty('email')
-        return memberInfo
 
-    def areAnyRequiredFieldsMissing(self, userInput):
+        self.username = member.getUserName()
+        self.fullname = member.getProperty('fullname')
+        if self.fullname == '':
+            self.fullname = self.username
+        self.email = member.getProperty('email')
+
+    def areAnyRequiredFieldsEmpty(self):
+        return len(self.getListOfEmptyRequiredFields()) > 0
+
+    def getListOfEmptyRequiredFields(self):
         fieldNames = self.context.schema['extraFields'].vocabulary
-        missingFields = []
-        anyMissing = False
-        if userInput['selectedSlots'] == [None]:
-            anyMissing = True
-            missingFields.append('Timeslot')
-            
-        extraFields = self.context.getExtraFields()
-        for field in extraFields:
-            if len(userInput[field]) < 1:
-                anyMissing = True
+        emptyRequiredFields = []
 
+        if self.selectedSlots == [None]:
+            emptyRequiredFields.append('Timeslot')
+            
+        requiredExtraFields = self.context.getExtraFields()
+        for field in requiredExtraFields:
+            if len(getattr(self, field)) < 1:
                 for (id, name) in fieldNames:
                     if id == field:
-                        missingFields.append(name)
+                        emptyRequiredFields.append(name)
                         break
 
-        return (anyMissing, missingFields)    	
+        return emptyRequiredFields
 
-    def createPerson(self, location, userInfo):
-        location.invokeFactory('Person', userInfo['username'])
-        newPerson = location[userInfo['username']]
-        newPerson.setEmail(userInfo['email'])
-        newPerson.setTitle(userInfo['fullname'])
-        newPerson.setClassification(userInfo['classification'])
-        newPerson.setDepartment(userInfo['dept'])
-        newPerson.setPhone(userInfo['phone'])
+    def getSlotAndSignUserUpForIt(self, slotLabel):
+        (signupSheetTitle, date, time) = slotLabel.split(' @ ')
+        signupSheet = self.context.getSignupSheet(signupSheetTitle)
+        day = signupSheet.getDay(date)
+        timeSlot = day.getTimeSlot(time)
+        allowWaitingList = timeSlot.getAllowWaitingList()
+        numberOfAvailableSpots = timeSlot.getNumberOfAvailableSpots()
+        
+        if allowWaitingList or numberOfAvailableSpots > 0:
+            person = self.createPersonObject(timeSlot)
+                
+            if numberOfAvailableSpots > 0:
+                self.signupPerson(person)
+                self.waiting = False
+            elif self.isEmailValid():
+                self.sendWaitingListConfirmationEmail(signupSheet, slotLabel)
+                    
+            self.success = True
+            
+        else:
+            self.errorMsg = 'The slot you selected is already full. Please select a different one'
+            self.success = False
+
+    def createPersonObject(self, container):
+        container.invokeFactory('Person', self.username)
+        newPerson = container[self.username]
+        newPerson.setEmail(self.email)
+        newPerson.setTitle(self.fullname)
+        for (field, _) in self.context.schema['extraFields'].vocabulary:
+            setattr(newPerson, field, getattr(self, field))
         newPerson.reindexObject()
         return newPerson
 
@@ -129,42 +122,22 @@ class ChooseTimeSlot(BrowserView):
         portal_workflow = getToolByName(self, 'portal_workflow')
         portal_workflow.doActionFor(person, 'signup')
         person.reindexObject()
+  
+    def isEmailValid(self):
+        isEmail = validation.validatorFor('isEmail')
+        return isEmail(self.email) == 1
 
-    def isAnyExtraInfoRequired(self):
-        return self.isPhoneRequired() or self.isDepartmentRequired() or self.isClassRequired()
-
-    def isPhoneRequired(self):
-        return self.isFieldRequired('phone')
-    
-    def isDepartmentRequired(self):
-        return self.isFieldRequired('dept')
-        
-    def isClassRequired(self):
-    	return self.isFieldRequired('classification')
-    
-    def isFieldRequired(self, field):
-    	extraFields = self.context.getExtraFields()
-        if field in extraFields:
-            return True
-        else:
-            return False
-    
-    def isCurrentUserLoggedIn(self):
-        portal_membership = getToolByName(self, 'portal_membership')
-        member = portal_membership.getAuthenticatedMember()
-        return 'Anonymous' not in member.getRoles()
-
-    def sendWaitingConfirmationEmail(self, userInfo, signupSheet, slot):
+    def sendWaitingListConfirmationEmail(self, signupSheet, slotLabel):
         if signupSheet.isContainedInMasterSignupSheet():
             signupSheet = signupSheet.aq_parent
 
         extraEmailContent = signupSheet.getExtraEmailContent()
         contactInfo = signupSheet.getContactInfo()
-        toEmail = userInfo['email']
+        toEmail = self.email
         fromEmail = "%s <%s>" % (self.context.email_from_name, self.context.email_from_address)
         subject = signupSheet.Title() + ' - Waiting List Confirmation'
         
-        message = 'Hi ' + userInfo['fullname'] + ',\n\n'
+        message = 'Hi ' + self.fullname + ',\n\n'
         message += 'This message is to confirm that you have been added to the waiting list for:\n'
         message += slot + '\n\n'
 
@@ -183,6 +156,21 @@ class ChooseTimeSlot(BrowserView):
 
         mailHost = self.context.MailHost
         mailHost.secureSend(message, toEmail, fromEmail, subject)
+
+    def areAnyExtraFieldsRequired(self):
+        return len(self.context.getExtraFields()) > 0
+
+    def isFieldRequired(self, field):
+    	extraFields = self.context.getExtraFields()
+        if field in extraFields:
+            return True
+        else:
+            return False
+    
+    def isCurrentUserLoggedIn(self):
+        portal_membership = getToolByName(self, 'portal_membership')
+        member = portal_membership.getAuthenticatedMember()
+        return 'Anonymous' not in member.getRoles()
 
     def getSlotsCurrentUserIsSignedUpFor(self):
         slots = []
