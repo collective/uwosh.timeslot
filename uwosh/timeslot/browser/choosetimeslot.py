8a12fa3e23f4bb5d3f13e43fa626a9970c6dc866
Marshall Scorcio <marshall.scorcio@gmail.com>
2009-07-17 20:53:04 +0000

    Cleaned up the page templates. And added some memoization

--- uwosh/timeslot/browser/choosetimeslot.py
+++ uwosh/timeslot/browser/choosetimeslot.py
@@ -2,6 +2,7 @@ from zope.interface import implements, Interface
 
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
+from plone.memoize import instance
 
 from uwosh.timeslot import timeslotMessageFactory as _
 
@@ -16,9 +17,11 @@ class ChooseTimeSlot(BrowserView):
         self.context = context
         self.request = request
 
+    @instance.memoize
     def areAnyExtraFieldsRequired(self):
         return len(self.context.getExtraFields()) > 0
 
+    @instance.memoize
     def isFieldRequired(self, field):
     	extraFields = self.context.getExtraFields()
         if field in extraFields:
@@ -26,11 +29,13 @@ class ChooseTimeSlot(BrowserView):
         else:
             return False
     
+    @instance.memoize
     def isCurrentUserLoggedIn(self):
         portal_membership = getToolByName(self, 'portal_membership')
         member = portal_membership.getAuthenticatedMember()
         return 'Anonymous' not in member.getRoles()
 
+    @instance.memoize
     def showEditLinks(self):
         portal_membership = getToolByName(self, 'portal_membership')
         member = portal_membership.getAuthenticatedMember()
