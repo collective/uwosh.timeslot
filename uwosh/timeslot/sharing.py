97decef94a4878d2e0bd5446f7dfa7c427f2e025
Servilio Afre Puentes <afrepues@mcmaster.ca>
2013-07-05 11:45:49 -0400

    Use the string directly as the i18n ID
    No advantage for the translation, but it makes reading the code a bit
    less easy. Also, for uniformity in the code, as the rest of the calls
    to the MessageFactory are done without an ID.

--- uwosh/timeslot/sharing.py
+++ uwosh/timeslot/sharing.py
@@ -4,10 +4,10 @@ from Products.CMFPlone import PloneMessageFactory as _
 
 class ScheduleViewerRole(object):
    implements(ISharingPageRole)
-   title = _(u"title_can_view_schedule", default=u"uwosh.timeslot: Can view schedule")
+   title = _(u"uwosh.timeslot: Can view schedule")
    required_permission = "uwosh.timeslot: Manage Schedule"
 
 class ScheduleManagerRole(object):
    implements(ISharingPageRole)
-   title = _(u"title_can_manage_schedule", default=u"uwosh.timeslot: Can manage schedule")
+   title = _(u"uwosh.timeslot: Can manage schedule")
    required_permission = "uwosh.timeslot: Manage Schedule"
